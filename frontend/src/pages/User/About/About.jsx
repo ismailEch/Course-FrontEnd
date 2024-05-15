0af9bc5a2e7c508a3f39caf84f30f47f166c7929
Soso0303 <ismailechchafiy56@gmail.com>
2024-05-15 13:02:32 +0100

    create  page about

---
+++ frontend/src/pages/User/About/About.jsx
@@ -0,0 +1,20 @@
+//  import React from 'react'
+import Navbar from "../../../components/User//Navbar";
+import HeroSection from "../../../components/User//Hero-section";
+import Footer from "../../../components/User//Footer";
+import AboutUs from '../../../components/User//AboutUs';
+
+function About() {
+    return (
+        <div>
+            <Navbar/>
+            <HeroSection/>
+            <AboutUs/>
+            <Footer/>
+        
+        </div>
+    )
+}
+
+export default About;
+
