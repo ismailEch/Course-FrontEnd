837f30ddb2cd9a9a7fbb55b7f9c5c06d4ee15618
Widad Elaydi <widadelaydi1998@gmail.com>
2024-05-15 16:23:50 +0100

    modified the about

--- frontend/src/components/User/EnrollPage.jsx
+++ frontend/src/components/User/EnrollPage.jsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import axios from 'axios';
 import profil8 from '../../assets/profil8.svg';
 
